ee5ed76cc63e0d3b7ed9d1b9ef2518211612a170
zonderneko <8taichou8@gmail.com>
2018-09-26 00:21:48 +0400

    comments removed

--- src/main/java/testCase/DropdownMenuURLs.java
+++ src/main/java/testCase/DropdownMenuURLs.java
@@ -1,11 +1,6 @@
 package testCase;
 
-import Utils.Constants;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.testng.Assert;
 import pageObject.Dropdown;
 
 public class DropdownMenuURLs {
@@ -15,16 +10,5 @@ public class DropdownMenuURLs {
         Dropdown.mainPage(driver);
         Dropdown dropdown = new Dropdown(driver);
         dropdown.assertChilds();
-//        Dropdown dropdown = new Dropdown(driver);
-//        for (String link : Constants.links) {
-//            Dropdown.moveTo(driver);
-//            WebElement element = dropdown.getElementByLinkPart(link);
-//            Actions action = new Actions(driver);
-//            action.moveToElement(element).build().perform();
-//            String s = element.getText();
-//            element.click();
-//            Assert.assertEquals(s, driver.findElement(By.xpath(Constants.headers)).getText());
-//        }
-
     }
 }
